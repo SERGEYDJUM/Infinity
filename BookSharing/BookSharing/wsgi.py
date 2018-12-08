1c3122e15f24cf504261e3e287eecf67242f2317
Sergey Djumageldiev <lutlut999@outlook.com>
2018-12-08 12:02:42 +0300

    added project

--- BookSharing/BookSharing/wsgi.py
+++ BookSharing/BookSharing/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BookSharing_dp project.
+WSGI config for BookSharing project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookSharing_dp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookSharing.settings')
 
 application = get_wsgi_application()
--- BookSharing/manage.py
+++ BookSharing/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookSharing_dp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookSharing.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
