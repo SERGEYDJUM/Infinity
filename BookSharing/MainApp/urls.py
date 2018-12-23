409f626a774d31a1a6b54fc0091065714e23c6f0
Alex228Sky <alex.sky.89@mail.ru>
2018-12-23 12:30:23 +0300

    login sigin
    login sigin

--- BookSharing/MainApp/urls.py
+++ BookSharing/MainApp/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signin', views.signin, name='signin'),
-    path('login', views.login, name='login'),
     path('about', views.about, name='about'),
     path('lib', views.lib, name='lib'),
     path('account', views.account, name='account')
--- BookSharing/MainApp/views.py
+++ BookSharing/MainApp/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+
 
 def index(reqest):
     return render(reqest,'index.html')
 
 def signin(reqest):
-    return render(reqest,'signin.html')
-
-def login(reqest):
-    return render(reqest,'login.html')
+    if reqest.method == 'POST':
+        form = UserRegisterForm(reqest.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(reqest, f'hi{username}')
+            return HttpResponseRedirect('/')
+    else:
+        form = UserRegisterForm()
+    return render(reqest,'registration/signin.html', {'form': form})
 
 def about(reqest):
     return render(reqest,'about.html')
