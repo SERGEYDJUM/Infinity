e0522b7c71bd2926bdff775a6acf2a3567218afe
Sergey Djumageldiev <lutlut999@outlook.com>
2018-12-26 21:13:04 +0300

    Remade header... again

--- BookSharing/MainApp/static/js/common.js
+++ BookSharing/MainApp/static/js/common.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {
   $(".sidenav").sidenav();
-  $('.dropdown-trigger').dropdown();
+  $('.dropdown-trigger').dropdown({constrainWidth: false, coverTrigger: false });
   $('.modal').modal();
 });
