a0f21e352552af513f94bc93bfd0c86baacf2308
Ilya Leontyev <ilyaleon1234567890@gmail.com>
2018-12-27 19:29:10 +1000

    Library - Updated
    Added floating action button

--- BookSharing/MainApp/static/js/common.js
+++ BookSharing/MainApp/static/js/common.js
@@ -3,4 +3,5 @@ $(document).ready(function()
   $(".sidenav").sidenav();
   $('.dropdown-trigger').dropdown({constrainWidth: false, coverTrigger: false });
   $('.modal').modal();
+  $('.fixed-action-btn').floatingActionButton();
 });
